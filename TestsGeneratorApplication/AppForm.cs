4ef2c4c67756f262a94b6b4a747540fec74f1e7a
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 17:54:59 +0300

    added aplication form

---
+++ TestsGeneratorApplication/AppForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestsGeneratorApplication
+{
+    public partial class AppForm : System.Windows.Forms.Form
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
