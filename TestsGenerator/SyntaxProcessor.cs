b738e51bb0411f6377eb8930c4ce7944a163b617
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 21:15:42 +0300

    added syntax processor class

---
+++ TestsGenerator/SyntaxProcessor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class СSharpSyntaxProcessor
+    {
+        public void Process(string sourceCode)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(sourceCode);
+        }
+    }
+}
