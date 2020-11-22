76c1119c6db3d793a91aaaa11336cb9b0e7429d6
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 23:43:43 +0300

    implemented generate method

--- TestsGenerator/TestsGenerator.cs
+++ TestsGenerator/TestsGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace TestsGenerator
 {
@@ -21,8 +22,47 @@ namespace TestsGenerator
             this.maxWriteFilesCount = maxWritableCount;
         }
 
-        public async Task Generate()
+        public async Task Generate(AsyncFileWriter writer)
         {
+            DataflowLinkOptions linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+            ExecutionDataflowBlockOptions maxReadableFilesTasks = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxReadFilesCount
+            };
+
+            ExecutionDataflowBlockOptions maxProcessableTasks = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxProcessTasksCount
+            };
+
+            ExecutionDataflowBlockOptions maxWritableTasks = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxWriteFilesCount
+            };
+
+            AsyncFileReader asyncReader = new AsyncFileReader();
+            TemplateClassGenerator templateGenerator = new TemplateClassGenerator();
+
+            TransformBlock<string, string> readingBlock =
+                new TransformBlock<string, string>(new Func<string, Task<String>>(asyncReader.Read), maxReadableFilesTasks);
+
+            TransformBlock<string, TestClassTemplate> processingBlock =
+                new TransformBlock<string, TestClassTemplate>(new Func<string, TestClassTemplate>(templateGenerator.GetTemplate), maxProcessableTasks);
+
+            ActionBlock<TestClassTemplate> writingBlock = new ActionBlock<TestClassTemplate>(
+                (classTemplate) => writer.Write(classTemplate), maxWritableTasks);
+
+            readingBlock.LinkTo(processingBlock, linkOptions);
+            processingBlock.LinkTo(writingBlock, linkOptions);
+
+            foreach (string filePath in fileNames)
+            {
+                readingBlock.Post(filePath);
+            }
+
+            readingBlock.Complete();
+
+            await writingBlock.Completion;
 
         }
     }
