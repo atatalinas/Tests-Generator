c028488d30eb09ccfd35c22b3cc6ebb9f163e56b
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 21:24:17 +0300

    added parsed information about class

---
+++ TestsGenerator/ClassInformation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class ClassInformation
+    {
+        string name;
+        string namespaceName;
+        List<string> methods;
+
+        public ClassInformation(string name, string nsName, List<string> methods)
+        {
+            this.name = name;
+            this.namespaceName = nsName;
+            this.methods = new List<string>(methods);
+        }
+
+        public string Name => name;
+        public string NamespaceName => namespaceName;
+        public List<string> Methods
+        {
+            get
+            {
+                return methods;
+            }
+            set
+            {
+                methods = new List<string>(value);
+            }
+        }
+    }
+}
