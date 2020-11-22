24bf61f45da9e85296b697c1f684f8e88a077a46
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 21:18:10 +0300

    added class for syntax process result

---
+++ TestsGenerator/SyntaxProcessResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class SyntaxProcessResult
+    {
+        List<string> classNames;
+        List<string> methodNames;
+
+        public List<string> ClassNames
+        {
+            get
+            {
+                return classNames;
+            }
+            set
+            {
+                classNames = new List<string>(value);
+            }
+        }
+
+        public List<string> MethodNames
+        {
+            get
+            {
+                return methodNames;
+            }
+            set
+            {
+                methodNames = new List<string>(value);
+            }
+        }
+    }
+}
