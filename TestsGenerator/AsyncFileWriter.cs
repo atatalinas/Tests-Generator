3339192cf7df0e9f2a021b711cdb7dcf8c7dd52f
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 23:34:09 +0300

    added async file writer class

---
+++ TestsGenerator/AsyncFileWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class AsyncFileWriter
+    {
+        private string outPutDirectory;
+
+        public AsyncFileWriter(string outPutDirectory)
+        {
+            this.outPutDirectory = outPutDirectory;
+        }
+
+        public async Task Write(TestClassTemplate classTemplate)
+        {
+            if (classTemplate == null)
+            {
+                return;
+            }
+            string filePath = outPutDirectory + "\\" + classTemplate.FileName;
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                await writer.WriteAsync(classTemplate.InnerText);
+            }
+        }
+    }
+}
