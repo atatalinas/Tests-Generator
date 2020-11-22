5bb8a43bc08a0653067ed4fe586754f041fdaea3
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 22:06:56 +0300

    added class for generated template

---
+++ TestsGenerator/TestClassTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class TestClassTemplate
+    {
+        string fileName;
+        string innerText;
+
+        public TestClassTemplate(string fileName, string innerText)
+        {
+            this.fileName = fileName;
+            this.innerText = innerText;
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+        }
+
+        public string InnerText
+        {
+            get { return innerText; }
+        }
+    }
+}
