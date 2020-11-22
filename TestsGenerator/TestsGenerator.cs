63cff060999aac8132a2003e6915c9af9f927c54
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-22 18:23:27 +0300

    added generator's class constructor

---
+++ TestsGenerator/TestsGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class TestsGenerator
+    {
+        private uint maxReadFilesCount;
+        private uint maxProcessTasksCount;
+        private uint maxWriteFilesCount;
+        private List<string> fileNames;
+
+        public TestsGenerator(List<string> fileNames, uint maxReadableCount, uint maxProcessCount, uint maxWritableCount)
+        {
+            this.fileNames = new List<string>(fileNames);
+            this.maxReadFilesCount = maxReadableCount;
+            this.maxProcessTasksCount = maxProcessCount;
+            this.maxWriteFilesCount = maxWritableCount;
+        }
+
+        public void Generate()
+        {
+
+        }
+    }
+}
